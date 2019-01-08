c183b0ec5817b823bfecedb6e59be9632961836c
BrendonGS <bgsggermany@gmail.com>
2019-01-08 17:31:50 -0600

    Item list

---
+++ store_item.rb
@@ -0,0 +1,7 @@
+#Each item has various properties such as color, price, etc.
+
+guns = {type: "revolver", make: "Colt", color: "blue", price: 700}
+socks = {:type => "ankle", :make => "Nike", :color => "black", price: 7}
+shoes = {type: "hi-top", make: "Jordan", color: "white", price: 7}
+
+
